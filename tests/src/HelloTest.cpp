3b28a090941be0f243b675161f80bd0527e2046e
Andrey Borisovich <business@borisovich.com>
2022-11-14 23:05:19 +0100

    Example usage of toml parser
    Added example parse function using tomlcplusplus library
    to World dummy class.
    Added two simple unit tests parsing toml from string.
    
    Signed-off-by: Andrey Borisovich <business@borisovich.com>

--- tests/src/HelloTest.cpp
+++ tests/src/HelloTest.cpp
@@ -18,6 +18,34 @@ TEST(HelloTest, HelloWorld)
 	EXPECT_STREQ(world.greet().data(), "Hello!");
 }
 
+TEST(HelloTest, TomlParser)
+{
+	static constexpr std::string_view some_toml = R"(
+		[library]
+		name = "toml++"
+		authors = ["Mark Gillard <mark.gillard@outlook.com.au>"]
+		cpp = 17
+	)";
+
+	fwsign::World world;
+
+	EXPECT_EQ(world.parse(some_toml), 0);
+}
+
+TEST(HelloTest, TomlParserBadFormat)
+{
+	static constexpr std::string_view some_toml = R"(
+		[library]]
+		name = "toml++"
+		authors = ["Mark Gillard <mark.gillard@outlook.com.au>"]
+		cpp = 17
+	)";
+
+	fwsign::World world;
+
+	EXPECT_EQ(world.parse(some_toml), 1);
+}
+
 }
 
 
